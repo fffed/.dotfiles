14894c25c5c0d663eb2d1f1e4c6281f9ff1fbe95
Fiodar Fiodarau <fiodar_fiodarau@epam.com>
2021-03-16 10:13:02 +0300

    add fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,60 @@
+#remove welcome message
+set fish_greeting
+
+# set fish_color_cwd dd9922
+# set fish_color dd9922
+set fish_color_autosuggestion 5E5959
+set fish_color_param dd9922
+
+#set full pathe in prompt
+set fish_prompt_pwd_dir_length 0
+
+#for neovim.appimage
+#set CUSTOM_NVIM_PATH /usr/local/bin/nvim.appimage
+#for neovim build from source
+set CUSTOM_NVIM_PATH /usr/local/bin/nvim
+
+#FZF
+set -U FZF_DEFAULT_OPTS "--height 50% --layout=reverse --border"
+# --multi
+# --preview-window=:hidden
+# --preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
+# --bind '?:toggle-preview'
+# --bind 'ctrl-a:select-all'"
+set -U FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always"
+set -U FZF_LEGACY_KEYBINDINGS 0
+set -U FZF_COMPLETE 1
+set -U FZF_ENABLE_OPEN_PREVIEW 1
+
+#Tide prompt
+set --global tide_git_bg_color 75A545
+set --global tide_pwd_bg_color 5E5959
+
+
+alias vit='vi --cmd "cd ~/telescope/telescope-frontend/frontend-suite/frontend-client" +pwd'
+alias vitc='vi --cmd "cd ~/telescope/telescope-frontend-core" +pwd'
+alias ll='ls -alhF'
+alias nnn='nnn -dH'
+alias dtf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+
+function print_fish_colors --description 'Shows the various fish colors being used'
+    set -l clr_list (set -n | grep fish | grep color | grep -v __)
+    if test -n "$clr_list"
+        set -l bclr (set_color normal)
+        set -l bold (set_color --bold)
+        printf "\n| %-38s | %-38s |\n" Variable Definition
+        echo '|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|'
+        for var in $clr_list
+            set -l def $$var
+            set -l clr (set_color $def ^/dev/null)
+            or begin
+                printf "| %-38s | %s%-38s$bclr |\n" "$var" (set_color --bold white --background=red) "$def"
+                continue
+            end
+            printf "| $clr%-38s$bclr | $bold%-38s$bclr |\n" "$var" "$def"
+        end
+        echo '|________________________________________|________________________________________|'\n
+    end
+end
+
