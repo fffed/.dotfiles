a9df2ed73a1237cb8030e8832584ccbe27fc9647
fffed@ex2540 <47778360+fffed@users.noreply.github.com>
2021-05-22 20:09:13 +0300

    add cheatsheet

---
+++ .config/sway/cheatsheet.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# search for "bindsym" that starts(^) from zero or more whitespaces(/s*)
+btn_pattern="^\s*bindsym"
+
+#grep $btn_pattern ./config | grep -v XF86 | sed "s/$btn_pattern //"
+
+#parse values with `awk` starting from second to vary end
+grep $btn_pattern ~/.config/sway/config | grep -v XF86 \
+    | sed "s/swaynag.*/'exit sway'/" \
+    | sed "s/\$term -a cheat.*/show keybindings/" \
+    | awk '{for(i=2;i<=NF;i++) if (i==2) printf $i"-- "; else printf $i" "; print ""}' \
+    | column -t -s '--' \
+    | less
