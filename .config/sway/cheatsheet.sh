822743efc2eaeacf353a9b4e9561267c76c17019
fffed <47778360+fffed@users.noreply.github.com>
2021-05-24 12:06:38 +0300

    exclude layout codes form cheatsheet

--- .config/sway/cheatsheet.sh
+++ .config/sway/cheatsheet.sh
@@ -9,6 +9,7 @@ btn_pattern="^\s*bindsym"
 grep $btn_pattern ~/.config/sway/config | grep -v XF86 \
     | sed "s/swaynag.*/'exit sway'/" \
     | sed "s/\$term -a cheat.*/show keybindings/" \
+    | sed "s/--to-code//" \
     | awk '{for(i=2;i<=NF;i++) if (i==2) printf $i"-- "; else printf $i" "; print ""}' \
     | column -t -s '--' \
     | less
