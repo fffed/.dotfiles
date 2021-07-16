e852d0fae230ce0bd21e69491ad9f650e8e03291
fffed <47778360+fffed@users.noreply.github.com>
2021-07-16 11:09:29 +0300

    improve wallpaper download

--- .config/sway/wallpapers.sh
+++ .config/sway/wallpapers.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-#check if wifi connection is metered
-if [[ $(nmcli -t -f GENERAL.METERED dev show wlp0s20f3) = 'GENERAL.METERED:no (guessed)' ]]; then
+#get active connection
+connected_device=$(nmcli -g device connection show --active)
+
+#check if connection is metered
+if [[ $(nmcli -t -f GENERAL.METERED dev show $connected_device) = 'GENERAL.METERED:no (guessed)' ]]; then
   #Store the wallpapers in /var/tmp/ instead of /tmp/, assuming that we want to keep these images in temporary but persistent folder
   temp_path="/var/tmp/"
   wl_path="${temp_path}wallpaper.jpg"
   lsw_path="${temp_path}wallpaper-lock.jpg"
+
   wl_url="https://source.unsplash.com/1920x1080"
   wget -O $wl_path $wl_url
+
   killall swaybg || true
   swaymsg "output * bg $wl_path fill"
 
