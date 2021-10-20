51f2c589609102abecaf37e1ecc9ca15065357b9
fffed <47778360+fffed@users.noreply.github.com>
2021-10-20 15:22:43 +0300

    add surfingkeys

---
+++ .config/surfingkeys/config.js
@@ -0,0 +1,6 @@
+// mapkey('<Ctrl-d>', 'Show me the money', function() {
+//     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
+// });
+
+map('<Ctrl-d', 'd');
+map('<Ctrl-u', 'e');
