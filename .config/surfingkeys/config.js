4033c0c385efb3a907eef4b8ad5c7f383a56f6ff
fffed <47778360+fffed@users.noreply.github.com>
2021-11-01 17:47:21 +0300

    add search engines

--- .config/surfingkeys/config.js
+++ .config/surfingkeys/config.js
@@ -4,3 +4,14 @@
 
 map('<Ctrl-d>', 'd');
 map('<Ctrl-u>', 'e');
+
+addSearchAliasX('b', 'brave', 'https://search.brave.com/search?source=web&q=', 's', 'https://search.brave.com/api/suggest?rich=true&source=web&q=', function(response) {
+    var res = JSON.parse(response.text);
+    return res[1];
+});
+
+addSearchAliasX('q', 'qwant', 'https://www.qwant.com/?t=web&q=', 's', 'https://api.qwant.com/v3/suggest?locale=en_GB&q=', function(response) {
+    var res = JSON.parse(response.text);
+    return res[data.items];
+});
+
