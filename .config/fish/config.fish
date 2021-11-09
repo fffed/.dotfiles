ef081f5ccfb7418b0c0d32048de27ecc1b5a5068
fffed <47778360+fffed@users.noreply.github.com>
2021-11-09 13:22:08 +0300

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,43 +3,60 @@
 
 # set fish_color_cwd dd9922
 # set fish_color dd9922
-set fish_color_autosuggestion 5E5959
-set fish_color_param dd9922
+#set fish_color_autosuggestion 5E5959
+#set fish_color_param B8A956
+# set fish_color_command 46BBF5
+#set fish_color_command 6DAC68 --bold
+#set fish_color_error ff4444
 
 #set full pathe in prompt
 set fish_prompt_pwd_dir_length 0
 
-#for neovim.appimage
-#set CUSTOM_NVIM_PATH /usr/local/bin/nvim.appimage
-#for neovim build from source
-# set CUSTOM_NVIM_PATH /usr/local/bin/nvim
-# set CUSTOM_NVIM_PATH /usr/local/bin/nvim
-# contains $HOME/sources/neovim/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/neovim/bin
 
-#FZF
-#set -U FZF_DEFAULT_OPTS "--height 50% --layout=reverse --border"
-# --multi
-# --preview-window=:hidden
-# --preview '([[ -f {} ]] && (bat --style=numbers --color=always {} || cat {})) || ([[ -d {} ]] && (tree -C {} | less)) || echo {} 2> /dev/null | head -200'
-# --bind '?:toggle-preview'
-# --bind 'ctrl-a:select-all'"
-# set -U FZF_PREVIEW_FILE_CMD "bat --style=numbers --color=always"
-# set -U FZF_LEGACY_KEYBINDINGS 0
-# set -U FZF_COMPLETE 1
-# set -U FZF_ENABLE_OPEN_PREVIEW 1
+#kitty theme HARDCORE
+set fish_color_param fd971f
+set fish_color_command a6e22e --bold
+set fish_color_error f92672
+
+#kitty theme SYMFONIC
+set fish_color_command 56db3a --bold
+
 
 #Tide prompt
-set --global tide_git_bg_color 6D9A40
-set --global tide_os_bg_color 7C947F
+# set --global tide_git_bg_color 6D9A40
+# set --global tide_os_bg_color 7C947F
+set --global tide_os_bg_color 878787
+set --global tide_git_bg_color 5f875f
 set --global tide_pwd_bg_color 5E5959
+# set --global tide_git_color_branch 4E9A06
 
-abbr -a -U ll ls -alhF
-abbr -a -U nnn nnn -dH
+
+#hydro promt
+#set --global hydro_color_git beed5f
+
+#abbr -a -U ll ls -AlhF
+#abbr -a -U nnn nnn -dH
+abbr -a -U cpu 'ps -eo pid,ppid,%cpu,%mem,cmd --sort=-%cpu | head -6'
+abbr -a -U mem 'ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | head -6'
+
+
+#EXA
+alias ls='exa'
+abbr -a -U ls ls --icons
+abbr -a -U ll ls -alFs type --icons
+
+#Configs
+abbr -a -U conf-vi nvim ~/.config/nvim/init.vim
+abbr -a -U conf-vi-plug nvim ~/.config/nvim/lua/plugins.lua
+abbr -a -U conf-fish nvim ~/.config/fish/config.fish
+abbr -a -U conf-kitty nvim ~/.config/kitty/kitty.conf
 
 alias vi='nvim'
 alias vit='nvim --cmd "cd ~/telescope/telescope-frontend/frontend-suite/frontend-client" +pwd'
 alias vitc='nvim --cmd "cd ~/telescope/telescope-frontend-core" +pwd'
+
 alias dtf='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias sscreen='~/scripts/screensharing.sh'
 
 
 function print_fish_colors --description 'Shows the various fish colors being used'
