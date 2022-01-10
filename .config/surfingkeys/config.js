15c78a6b306df068aa04d19321f77676e9f9511b
fffed <47778360+fffed@users.noreply.github.com>
2022-01-10 10:45:25 +0300

    refactor api

--- .config/surfingkeys/config.js
+++ .config/surfingkeys/config.js
@@ -1,22 +1,26 @@
-// mapkey('<Ctrl-d>', 'Show me the money', function() {
-//     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
-// });
+const {
+    aceVimMap,
+    map,
+    addSearchAlias,
+    Hints,
+    Visual,
+} = api;
 
-api.map('<Ctrl-d>', 'd');
-api.map('<Ctrl-u>', 'e');
+map('<Ctrl-d>', 'd');
+map('<Ctrl-u>', 'e');
 
 // MAPPINGS
 // Create mapping in vim editor
-api.aceVimMap('kj', '<Esc>', 'insert');
+aceVimMap('kj', '<Esc>', 'insert');
 
-api.addSearchAlias('b', 'brave', 'https://search.brave.com/search?source=web&q=', 's', 'https://search.brave.com/api/suggest?rich=true&source=web&q=', function(response) {
+addSearchAlias('b', 'brave', 'https://search.brave.com/search?source=web&q=', 's', 'https://search.brave.com/api/suggest?rich=true&source=web&q=', function(response) {
     var res = JSON.parse(response.text);
     return res[1].map(function(r) {
         return r.q;
     });
 });
 
-api.addSearchAlias('w', 'qwant', 'https://www.qwant.com/?t=web&q=', 's', 'https://api.qwant.com/v3/suggest?locale=en_GB&q=', function(response) {
+addSearchAlias('w', 'qwant', 'https://www.qwant.com/?t=web&q=', 's', 'https://api.qwant.com/v3/suggest?locale=en_GB&q=', function(response) {
     var res = JSON.parse(response.text);
     return res.data.items.map(function(r) {
         return r.value;
