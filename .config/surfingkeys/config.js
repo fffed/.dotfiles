a8fa33477ab78cab7bb550191fe3a13f9e030170
fffed <47778360+fffed@users.noreply.github.com>
2022-01-10 10:17:19 +0300

    add api to map

--- .config/surfingkeys/config.js
+++ .config/surfingkeys/config.js
@@ -2,8 +2,8 @@
 //     Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
 // });
 
-map('<Ctrl-d>', 'd');
-map('<Ctrl-u>', 'e');
+api.map('<Ctrl-d>', 'd');
+api.map('<Ctrl-u>', 'e');
 
 // MAPPINGS
 // Create mapping in vim editor
